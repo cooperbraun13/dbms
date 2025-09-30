aa9d1425c550a77b4831b9f36a02caa8ca26652c
cooperbraun13 <cooperjbraun13@gmail.com>
2025-09-30 00:30:17 -0700

    setup hw3

---
+++ hw3/hw3_1.sql
@@ -0,0 +1,22 @@
+/*======================================================================
+ * 
+ *  NAME:    Cooper Braun
+ *  ASSIGN:  HW-3, Part 1
+ *  COURSE:  CPSC 321, Fall 2025
+ *  DESC:    ... description ....
+ * 
+ *======================================================================*/
+
+-- start clean each run
+DROP TABLE IF EXISTS City     CASCADE;
+DROP TABLE IF EXISTS Province CASCADE;
+DROP TABLE IF EXISTS Border   CASCADE;
+DROP TABLE IF EXISTS Country  CASCADE;
+
+-- TODO:
+--   * Fill in your name above and a brief description.
+--   * Implement the Part 1 schema as per the homework instructions.
+--   * Populate each table according to the homework instructions.
+--   * Be sure each table has a comment describing its purpose.
+--   * Be sure to add comments as needed for attributes.
+--   * Be sure your SQL code is well formatted (according to the style guides).
