e7ec71d98a96d4ff15a769869af6aa99efe0d829
cooperbraun13 <cooperjbraun13@gmail.com>
2025-09-30 00:38:37 -0700

    finished first table

--- hw3/hw3_1.sql
+++ hw3/hw3_1.sql
@@ -13,6 +13,22 @@ DROP TABLE IF EXISTS Province CASCADE;
 DROP TABLE IF EXISTS Border   CASCADE;
 DROP TABLE IF EXISTS Country  CASCADE;
 
+-- first, the schemas
+
+-- countries: two-letter code, name, GDP per capita (USD), inflation (%)
+CREATE TABLE Country (
+  country_code CHAR(2),
+  name         VARCHAR(50) NOT NULL,
+  gdp          VARCHAR(10) NOT NULL,
+  inflation    VARCHAR(10) NOT NULL,
+  PRIMARY KEY (country_code),
+  CONSTRAINT valid_gdp CHECK (gdp >= 0),
+  -- can have 'negative inflation' but that would be called deflation
+  CONSTRAINT valid_inflation CHECK (inflation >= 0)
+);
+
+-- provinces
+
 -- TODO:
 --   * Fill in your name above and a brief description.
 --   * Implement the Part 1 schema as per the homework instructions.
