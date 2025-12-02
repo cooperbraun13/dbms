5a2afb723a9cb8845866f07e4a7d8b83fd771541
cooperbraun13 <cooperjbraun13@gmail.com>
2025-12-02 01:01:44 -0800

    feat: setup hw10

---
+++ hw10/hw10_A.sql
@@ -0,0 +1,12 @@
+/*======================================================================
+ * 
+ *  NAME:    Cooper Braun
+ *  ASSIGN:  HW-10
+ *  COURSE:  CPSC 321, Fall 2025
+ *  DESC:    
+ *           
+ *======================================================================*/
+
+ /* 
+(1) Purpose: Find actors that have not acted in any 'PG' rated film
+*/
