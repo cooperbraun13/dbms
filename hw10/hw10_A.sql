cb15c3984898c5b6fd84a94bc9dd2ca85b8cf961
cooperbraun13 <cooperjbraun13@gmail.com>
2025-12-03 17:07:34 -0800

    feat: finished 1-4 (part 1) of hw10

--- hw10/hw10_A.sql
+++ hw10/hw10_A.sql
@@ -44,3 +44,45 @@ SELECT
 FROM film
 ORDER BY film_id
 LIMIT 10;
+
+ /* 
+(3) Purpose: Ranks films within each rating by length using a window function, where a shorter film has a higher rank.
+             Ties receive the same length; next distinct length gets the next integer rank. Limited to 10 rows
+             due to the output being very long
+*/
+
+SELECT
+  film_id, 
+  title, 
+  rating, 
+  length, 
+  DENSE_RANK() OVER (PARTITION BY rating ORDER BY length ASC) AS rank
+FROM film
+ORDER BY rating, rank, film_id
+LIMIT 10;
+
+ /* 
+(4) Purpose: Use a CTE that unifies actor, category, and inventory updates for all films, then return only those updates
+             for Action films. Each update is labeled with its type. Limited to 10 rows due to the output being very long
+*/
+
+WITH film_update AS (
+  -- actor updates
+  SELECT DISTINCT fa.film_id, 'actor' AS type, fa.last_update
+  FROM film_actor fa
+  UNION
+  -- category updates
+  SELECT DISTINCT fc.film_id, 'category' AS type, fc.last_update
+  FROM film_category fc
+  UNION
+  -- inventory updates
+  SELECT DISTINCT iv.film_id, 'inventory' AS type, iv.last_update
+  FROM inventory iv
+)
+SELECT fu.film_id, fi.title, fu.type, fu.last_update
+FROM film_update fu
+JOIN film fi ON fu.film_id = fi.film_id
+JOIN film_category fc ON fi.film_id = fc.film_id
+JOIN category ca ON fc.category_id = ca.category_id
+WHERE ca.name = 'Action'
+ORDER BY fi.title, fu.last_update
